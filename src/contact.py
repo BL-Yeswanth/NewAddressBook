d6846e9320ecbc883191ff7e1f8ac9ca00f70028
BL-Yeswanth <yeswanth.polisetti@bridgelabz.com>
2026-01-07 20:46:33 +0530

    [yeswanth] Added:UC1: Implement create contact functionality

---
+++ src/contact.py
@@ -0,0 +1,24 @@
+
+#  UC1 : Ability to create a contact
+
+class Contact:
+    def __init__(self, first_name, last_name, address, city,
+                 state, zip_code, phone_number, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.phone_number = phone_number
+        self.email = email
+
+    def display(self):
+        print("First Name :", self.first_name)
+        print("Last Name  :", self.last_name)
+        print("Address    :", self.address)
+        print("City       :", self.city)
+        print("State      :", self.state)
+        print("Zip        :", self.zip_code)
+        print("Phone No   :", self.phone_number)
+        print("Email      :", self.email)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,23 @@
+from contact import Contact
+
+
 class AddressBookMain:
     def start(self):
         print("Welcome to Address Book Program")
+        
+        # UC1 : Ability to create a contact
+        contact = Contact(
+            first_name=input("Enter First Name: "),
+            last_name=input("Enter Last Name: "),
+            address=input("Enter Address: "),
+            city=input("Enter City: "),
+            state=input("Enter State: "),
+            zip_code=input("Enter Zip Code: "),
+            phone_number=input("Enter Phone Number: "),
+            email=input("Enter Email: ")
+        )
+
+        contact.display()
 
 
 if __name__ == "__main__":
